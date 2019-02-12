9bf37d15d302f7b787564dc343744a832a0c9eda
RikSagara <www.riksagara@gmail.com>
2019-02-12 21:21:08 +0300

    start git

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="keywords" content="вирява эрзя мордва масторава стихи переводы поэмы " />
+    <meta name="description" content="описание сайта" />
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="img/icon.ico" rel="shortcut icon" type="image/x-icon" />
+    <title>РАВ</title>
+</head>
+<body>
+
+</body>
+</html>
