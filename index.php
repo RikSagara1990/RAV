fe46f1371e7ec771b8a3b03ec5c4194ad24241f1
RikSagara <www.riksagara@gmail.com>
2019-02-26 22:01:22 +0300

    26.02.2019

--- index.php
+++ index.php
@@ -10,65 +10,46 @@
 </head>
 <body>
     <header>
-        <div id="head">
-                <a id="logo" href="index.php" title="перейти на главную станицу">
-                    <span>РАВ</span>
-                </a>
-                <div class="options" id="user">
-                    <a id="adddocument">
-                        <span>Создать</span>
-                        <img src="img/adddocument.svg" alt="Создать">
-                    </a>
-                </div>
-                <div class="options" id="main">
-                    <a id="search">
-                        <span>Поиск</span>
-                        <img src="img/iconsearch.svg" alt="Поиск"> 
-                    <a id="enterinprofile">
-                        <span>Вход</span>
-                        <img src="img/iconenterprofile.svg" alt="Вход в личный кабинет">
-                    </a>
-                </div>
+        <a id="logo" href="index.php" title="перейти на главную станицу">
+            <span>РАВ</span>
+        </a>
+        <div class="options" id="user">
+            <a id="adddocument">
+                <span>Создать</span>
+                <img src="img/adddocument.svg" alt="Создать">
+            </a>
         </div>
-        <nav>
-            <a class="menu" id="menuoption1">
-                <span>Эпос Масторава</span>
+        <div class="options" id="main">
+            <a id="search">
+                <span>Поиск</span>
+                <img src="img/iconsearch.svg" alt="Поиск"> 
+            <a id="enterinprofile">
+                <span>Вход</span>
+                <img src="img/iconenterprofile.svg" alt="Вход в личный кабинет">
             </a>
-            <a class="menu">
-                <span>Корнеслов</span>
-            </a>
-            <a class="menu">
-                <span>История</span>
-            </a>
-            <a class="menu">
-                <span>Археология</span>
-            </a>
-            <a class="menu">
-                <span>Этнография Традиции Обряды</span>
-            </a>
-            <a class="menu">
-                <span>Нацинальные костюмы</span>
-            </a>
-            <a class="menu">
-                <span>Прикладное искусство</span>
-            </a>
-            <a class="menu">
-                <span>Изобразительное искусство</span>
-            </a>
-            <a class="menu">
-                <span>Литература</span>
-            </a>
-            <a class="menu" id="menuoption10">
-                <span>Конкурсы</span>
-            </a>
-        </nav>
-        <input type="image" id="showmenu" src="img/tongue.svg" alt="Меню">
+        </div>
     </header>
+    <nav>
+        <ul>
+            <li id="menustart"><a class="menu">Эпос Масторава</a></li>
+            <li><a class="menu">Корнеслов</a></li>
+            <li><a class="menu">История</a></li>
+            <li><a class="menu">Археология</a></li>
+            <li><a class="menu">Этнография Традиции Обряды</a></li>
+            <li><a class="menu">Нацинальные костюмы</a></li>
+            <li><a class="menu">Прикладное искусство</a></li>
+            <li><a class="menu">Изобразительное искусство</a></li>
+            <li><a class="menu">Литература</a></li>
+            <li id="menuend"><a class="menu">Конкурсы</a></li> 
+        </ul>
+        <input type="image" id="showmenu" src="img/tongue.svg" alt="Меню">
+    </nav>     
+ 
     <main>
-        <figure class="mainborder" id="leftborder"></figure>   
+        <div class="mainborder" id="leftborder"></div>
+        
         <div class="elementmain" id="description">Полное описание сайта Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</div>
-
-        <figure class="mainborder" id="rightborder"></figure>
+        <div class="mainborder" id="rightborder"></div>
     </main>
 
     <footer>
